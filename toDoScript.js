70903bfaafc34ef2495cda6623f04cf2faae8cae
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-18 18:49:57 +0530

    Added toggleCompleted() and toggleAll() methods

--- toDoScript.js
+++ toDoScript.js
@@ -1,18 +1,66 @@
-let toDoList = {
-  todos: ["item1", "item2", "item3"],
+let todoList = {
+  todos: [],
+  // todos: [{todoText: ..., completed: false}, {todoText: ..., completed: false}]
+
   displayTodos: function () {
-    console.log("My Todos:", this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your To-Do list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (let i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed) {
+          console.log("(X)", this.todos[i].todoText);
+        } else {
+          console.log("( )", this.todos[i].todoText);
+        }
+      }
+    }
   },
-  addTodo: function (todo) {
-    this.todos.push(todo);
+
+  addTodo: function (todoInput) {
+    this.todos.push({
+      todoText: todoInput,
+      completed: false,
+    });
     this.displayTodos();
   },
-  changeTodo: function (position, newValue) {
-    this.todos[position] = newValue;
+
+  changeTodo: function (position, newTodo) {
+    this.todos[position].todoText = newTodo;
     this.displayTodos();
   },
+
   deleteTodo: function (position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+
+  toggleCompleted: function (position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+
+  toggleAll: function () {
+    let totalTodos = this.todos.length;
+    completedTodos = 0;
+    for (let i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed) {
+        completedTodos++;
+      }
+    }
+    //if everything true, turn everything false
+    if (completedTodos === totalTodos) {
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    }
+    //otherwise, turn everything true
+    else {
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
     this.displayTodos();
   },
 };
